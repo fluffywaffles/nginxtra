d2b956d0ed142a3c445ce568b0181331ab02c865
fluffywaffles <skorlir@gmail.com>
2020-07-07 22:05:40 -0400

    Refactor: always use braces `${...}` for variable references

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -5,17 +5,17 @@ if [[ ! $(git status) ]]; then
 fi
 
 git_root=$(git rev-parse --show-toplevel)
-run_conf="$git_root/run/nginx.conf"
-if [[ ! -e $run_conf ]]; then
-  >&2 echo "✗ $run_conf does not exist!"
+run_conf="${git_root}/run/nginx.conf"
+if [[ ! -e ${run_conf} ]]; then
+  >&2 echo "✗ ${run_conf} does not exist!"
   >&2 echo "! try enabling a configuration."
   exit 1
 fi
 nginx_command=$(
-  printf 'nginx -c %s -g %s'                     \
-    $run_conf                                    \
-    "'daemon off; pid $git_root/run/nginx.pid;'"
+  printf 'nginx -c %s -g %s'                       \
+    ${run_conf}                                    \
+    "'daemon off; pid ${git_root}/run/nginx.pid;'"
 )
 
 set -x # print commands when they are run
-watchexec -s SIGHUP --no-vcs-ignore -f '*.conf' "$nginx_command"
+watchexec -s SIGHUP --no-vcs-ignore -f '*.conf' "${nginx_command}"
